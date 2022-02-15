399344315201032f504b23871989816b9a719007
Sungjoong Kim <sjjoseph106@gmail.com>
2022-02-15 14:36:23 +0900

    Time: 105 ms (31.10%), Space: 42.1 MB (21.42%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.js
+++ 35-search-insert-position/35-search-insert-position.js
@@ -4,16 +4,10 @@
  * @return {number}
  */
 const searchInsert = function (nums, target) {
-  // 배열 내에 존재하는 경우
-  if (nums.indexOf(target) !== -1) {
-    return nums.indexOf(target);
-  }
-  // 배열 내에 존재하지 않는 경우
   for (let i = 0; i < nums.length; i++) {
-    if ((nums[i] < target) & (target < nums[i + 1])) {
-      return i + 1;
+    if (nums[i] >= target) {
+      return i;
     }
   }
-  // 처음이나 끝에 위치하는 경우
-  return target < nums[0] ? 0 : nums.length;
+  return nums.length;
 };
