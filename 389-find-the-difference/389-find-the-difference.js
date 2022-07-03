4323ea32c108304ceee02f2258b110e470e506f0
Sungjoong Kim <sjjoseph106@gmail.com>
2022-07-03 15:11:19 +0900

    Time: 105 ms (46.94%), Space: 43.5 MB (76.74%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+const findTheDifference = function(s, t) {
+    s = s.split("").sort();
+    t = t.split("").sort();
+    for (let i = 0; i < s.length; i++) if (s[i] !== t[i]) return t[i];
+    return t[t.length-1];
+};
