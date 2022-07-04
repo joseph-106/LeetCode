10e026095216fdd021ede875d1164e098b3dfd8d
Sungjoong Kim <sjjoseph106@gmail.com>
2022-07-04 13:58:05 +0900

    Time: 2886 ms (5.30%), Space: 48.8 MB (29.59%) - LeetHub

--- 645-set-mismatch/645-set-mismatch.js
+++ 645-set-mismatch/645-set-mismatch.js
@@ -3,17 +3,11 @@
  * @return {number[]}
  */
 const findErrorNums = function(nums) {
-    const resultArr = [];
-    const existArr = [];
-    let tmp;
-    for(let i = 1; i <= nums.length; i++){
-        if (existArr.includes(nums[i-1])){
-            resultArr.push(nums[i-1]);
-        } else {
-            existArr.push(nums[i-1]);
-        }
-        if(!nums.includes(i)) tmp = i;
-    }
-    resultArr.push(tmp);
+    const resultArr = new Array(2);
+    for (let i = 1; i <= nums.length; i++) {
+        if (!isNaN(resultArr[0]) && !isNaN(resultArr[1])) break;
+        if (nums.filter(e => e === i).length === 2) resultArr[0] = i;
+        if (!nums.includes(i)) resultArr[1] = i; 
+    };
     return resultArr;
 };
