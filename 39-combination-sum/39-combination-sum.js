9b8c752f455a34a96905958de675f58fad199a76
Sungjoong Kim <sjjoseph106@gmail.com>
2022-07-08 16:06:00 +0900

    Time: 103 ms (75.94%), Space: 48.4 MB (35.67%) - LeetHub

--- 39-combination-sum/39-combination-sum.js
+++ 39-combination-sum/39-combination-sum.js
@@ -7,7 +7,7 @@ const combinationSum = function(candidates, target) {
     const resultArr = [];
     const recursive = (sum, sumArr, temp) => {
         if (sum === target) resultArr.push(sumArr);
-        if (sum < target) for (let i = temp; i < candidates.length; i++) recursive(sum + candidates[i], [...sumArr, candidates[i]], i);
+        if (sum < target) for (let i = temp; i < candidates.length; i++) if (sum + candidates[i] <= target) recursive(sum + candidates[i], [...sumArr, candidates[i]], i);
     };
     recursive(0, [], 0);
     return resultArr;
