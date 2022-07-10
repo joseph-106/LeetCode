17924c41a2fa35d86ca5a358ba9863f587d22e28
Sungjoong Kim <sjjoseph106@gmail.com>
2022-07-10 15:06:43 +0900

    Time: 90 ms (59.33%), Space: 42.4 MB (88.97%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
@@ -3,9 +3,6 @@
  * @return {number}
  */
 const minCostClimbingStairs = function(cost) {
-    let dy = Array.from({ length: cost.length }, () => 0);
-    dy[0] = cost[0];
-    dy[1] = cost[1];
-    for (let i = 2; i < dy.length; i++) dy[i] = Math.min(dy[i-2] + cost[i], dy[i-1] + cost[i]);
-    return Math.min(dy[dy.length - 2], dy[dy.length - 1]);
+    for (let i = 2; i < cost.length; i++) cost[i] = Math.min(cost[i-2] + cost[i], cost[i-1] + cost[i]);
+    return Math.min(cost.pop(), cost.pop());
 };
