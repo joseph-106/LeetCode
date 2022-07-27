e9fbb8b6e02eb50b6e4504cdd6f605590ecc00da
Sungjoong Kim <sjjoseph106@gmail.com>
2022-07-27 14:18:25 +0900

    Time: 72 ms (91.87%), Space: 44.6 MB (39.12%) - LeetHub

--- 169-majority-element/169-majority-element.js
+++ 169-majority-element/169-majority-element.js
@@ -3,10 +3,5 @@
  * @return {number}
  */
 const majorityElement = function(nums) {
-    let count = 1;
-    nums.sort((a, b) => a - b);
-    for (let i = 0; i < nums.length ; i++) {
-        (nums[i] === nums[i+1]) ? count++ : count = 1;
-        if (count > nums.length/2) return nums[i];
-    }
+    return nums.sort((a, b) => a - b)[Math.floor(nums.length/2)];
 };
