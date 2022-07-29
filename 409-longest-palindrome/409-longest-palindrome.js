b3818b073234bd2859c0e5ddae658ed2b3419482
Sungjoong Kim <sjjoseph106@gmail.com>
2022-07-30 04:03:33 +0900

    Time: 100 ms (52.17%), Space: 44.3 MB (38.67%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.js
+++ 409-longest-palindrome/409-longest-palindrome.js
@@ -3,18 +3,15 @@
  * @return {number}
  */
 const longestPalindrome = function(s) {
-    const tempObj = {};
-    for (const ch of s) tempObj[ch] ? tempObj[ch]++ : tempObj[ch] = 1;
-    const tempArr = Object.values(tempObj);
     let longest = 0;
-    let flag = true;
-    tempArr.forEach(e => {
-        if (e % 2 === 0) {
-            longest += e;
+    const tempSet = new Set();
+    for (const ch of s) {
+        if (tempSet.has(ch)) {
+            longest += 2;
+            tempSet.delete(ch);
         } else {
-            longest += e - 1;
-            flag = false;
+            tempSet.add(ch);
         }
-    })
-    return flag ? longest : longest + 1;
+    }
+    return tempSet.size > 0 ? longest + 1 : longest;
 };
