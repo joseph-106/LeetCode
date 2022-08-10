ad7c2b03d0859a265969eaed414b274b6c10f395
Sungjoong Kim <sjjoseph106@gmail.com>
2022-08-10 14:42:58 +0900

    Time: 188 ms (61.47%), Space: 48.9 MB (33.09%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.js
+++ 12-integer-to-roman/12-integer-to-roman.js
@@ -4,55 +4,12 @@
  */
 const intToRoman = function(num) {
     let res = "";
-    if (num >= 1000) {
-        const temp = Math.floor(num / 1000);
-        num -= temp * 1000;
-        res += "M".repeat(temp);
-    }
-    if (num >= 500) {
-        if (num >= 900) {
-            num -= 900;
-            res += "CM"
-        } else {
-            num -= 500;
-            res += "D";
+    const hashObj = {1000: "M", 900: "CM", 500: "D", 400: "CD", 100: "C", 90: "XC", 50: "L", 40: "XL", 10: "X", 9: "IX", 5: "V", 4: "IV", 1: "I"};
+    Object.keys(hashObj).reverse().forEach(e => {
+        while (num >= e) {
+            num -= e;
+            res += hashObj[e];
         }
-    }
-    if (num >= 100) {
-        const temp = Math.floor(num / 100);
-        num -= temp * 100;
-        if (temp === 4) res += "CD";
-        else res += "C".repeat(temp);
-    }
-    if (num >= 50) {
-        if (num >= 90) {
-            num -= 90;
-            res += "XC"
-        } else {
-            num -= 50;
-            res += "L";
-        }
-    }
-    if (num >= 10) {
-        const temp = Math.floor(num / 10);
-        num -= temp * 10;
-        if (temp === 4) res += "XL";
-        else res += "X".repeat(temp);
-    }
-    if (num >= 5) {
-        if (num >= 9) {
-            num -= 9;
-            res += "IX"
-        } else {
-            num -= 5;
-            res += "V";
-        }
-    }
-    if (num >= 1) {
-        const temp = num / 1;
-        num -= temp * 1;
-        if (temp === 4) res += "IV";
-        else res += "I".repeat(temp);
-    }
+    })
     return res;
-};
+}; 
