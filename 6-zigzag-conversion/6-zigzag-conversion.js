a4198e555c2a81da6937c9210c65e661a3396746
Sungjoong Kim <sjjoseph106@gmail.com>
2022-08-13 23:10:40 +0900

    Time: 102 ms (82.48%), Space: 45.8 MB (89.75%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.js
+++ 6-zigzag-conversion/6-zigzag-conversion.js
@@ -4,16 +4,15 @@
  * @return {string}
  */
 const convert = function(s, numRows) {
-    let result = "";
-    let num = 1;
-    let flag = true;
-    const tempArr = [];
     if (numRows === 1) return s;
-    for (let i = 0; i < s.length; i++){
-        tempArr.push([s[i], num]);
-        num += flag ? 1 : -1;
-        if (num === numRows || num === 1) flag = !flag;
+    const tempArr = new Array(numRows).fill("");
+    let loc = 0;
+    let flag = true;
+    for (let i = 0; i < s.length; i++) {
+        tempArr[loc] += s[i];
+        flag ? loc++ : loc--;
+        if (loc === numRows - 1) flag = false;
+        else if (loc === 0) flag = true;
     }
-    for (let i = 1; i <= numRows; i++) tempArr.forEach(e => {if (e[1] === i) result += e[0]});
-    return result;
+    return tempArr.join("");
 };
