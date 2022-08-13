bc9b3ca5dec0c0e04999c9854dbca6bc30a6d088
Sungjoong Kim <sjjoseph106@gmail.com>
2022-08-14 00:00:01 +0900

    Time: 935 ms (20.26%), Space: 46.7 MB (64.71%) - LeetHub

--- 888-fair-candy-swap/888-fair-candy-swap.js
+++ 888-fair-candy-swap/888-fair-candy-swap.js
@@ -4,11 +4,11 @@
  * @return {number[]}
  */
 const fairCandySwap = function(aliceSizes, bobSizes) {
-    const aliceSum = aliceSizes.reduce((acc, cur) => acc + cur, 0);
-    const bobSum = bobSizes.reduce((acc, cur) => acc + cur, 0);
-    for (let i = 0; i < aliceSizes.length; i++){
-        for (let j = 0; j < bobSizes.length; j++){
-            if (aliceSum - aliceSizes[i] + bobSizes[j] === bobSum - bobSizes[j] + aliceSizes[i]) return [aliceSizes[i], bobSizes[j]];
+    const sumA = aliceSizes.reduce((acc, cur) => acc + cur, 0);
+    const sumB = bobSizes.reduce((acc, cur) => acc + cur, 0);
+    for (let i = 0; i < aliceSizes.length; i ++) {
+        for (let j = 0; j < bobSizes.length; j++) {
+            if (sumA + (bobSizes[j] * 2) === sumB + (aliceSizes[i] * 2)) return [aliceSizes[i], bobSizes[j]];
         }
     }
 };
