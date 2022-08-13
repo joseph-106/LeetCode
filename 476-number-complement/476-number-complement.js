51e61f1e8411641fafb7c288339d9d0839b80510
Sungjoong Kim <sjjoseph106@gmail.com>
2022-08-14 00:08:47 +0900

    Time: 63 ms (87.88%), Space: 41.7 MB (87.37%) - LeetHub

--- 476-number-complement/476-number-complement.js
+++ 476-number-complement/476-number-complement.js
@@ -3,5 +3,5 @@
  * @return {number}
  */
 const findComplement = function(num) {
-    return parseInt(num.toString(2).split("").reduce((acc, cur) => { acc += cur === "0" ? "1" : "0"; return acc }, ""), 2);
+    return parseInt([...num.toString(2)].map((e) => e === "0" ? 1 : 0).join(""), 2);
 };
