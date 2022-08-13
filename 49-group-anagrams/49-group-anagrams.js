4507a1085efd3ac0902ed7c3e8eb56603d8fad66
Sungjoong Kim <sjjoseph106@gmail.com>
2022-08-13 13:48:10 +0900

    Time: 132 ms (91.99%), Space: 52.4 MB (90.58%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.js
+++ 49-group-anagrams/49-group-anagrams.js
@@ -2,23 +2,12 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
-const groupAnagrams = function (strs) {
-  const answer = [];
-  const temp = [];
-  for (let i = 0; i < strs.length; i++) {
-    // 배열로 쪼개서 정렬하고 다시 합치기
-    let sort = strs[i].split('').sort().join('');
-    if (temp.includes(sort)) {
-      // 임시 배열에 정렬된 값이 있다면
-      // 정답의 해당 위치 배열에 push
-      answer[temp.indexOf(sort)].push(strs[i]);
-    } else {
-      // 임시 배열에 정렬된 값이 없다면
-      // 정답에 신규 배열 push
-      answer.push([strs[i]]);
-      // 임시 배열에 정렬된 값 push
-      temp.push(sort);
-    }
-  }
-  return answer;
+const groupAnagrams = function(strs) {
+    const hashObj = {};
+    strs.forEach(e => {
+        const sorted = e.split("").sort().join("");
+        if (hashObj[sorted]) hashObj[sorted].push(e);
+        else hashObj[sorted] = [e];
+    })
+    return Object.values(hashObj);
 };
