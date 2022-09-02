9658b8f73e538cf663be0096592e3585a08f0220
Sungjoong Kim <sjjoseph106@gmail.com>
2022-09-03 01:10:28 +0900

    Time: 3593 ms (7.79%), Space: 45.2 MB (85.71%) - LeetHub

--- 962-maximum-width-ramp/962-maximum-width-ramp.js
+++ 962-maximum-width-ramp/962-maximum-width-ramp.js
@@ -5,8 +5,7 @@
 const maxWidthRamp = function(nums) {
     let max = 0;
     for (let i = 0; i < nums.length; i++) {
-        for (let j = nums.length - 1; j > i; j--) {
-            if (j - i <= max) break;
+        for (let j = nums.length - 1; j > i + max; j--) {
             if (nums[i] <= nums[j]) max = Math.max(max, j - i);
         }
     }
