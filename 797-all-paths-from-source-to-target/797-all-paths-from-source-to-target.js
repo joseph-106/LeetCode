c33399eb700c5bb8bd4ff01647af719220b75fec
Sungjoong Kim <sjjoseph106@gmail.com>
2022-09-04 19:19:02 +0900

    Time: 161 ms (52.21%), Space: 52.2 MB (32.46%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.js
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.js
@@ -5,11 +5,9 @@
 const allPathsSourceTarget = function(graph) {
     const allArr = [];
     const recursive = (loc, tempArr) => {
-        if (loc === graph.length - 1) allArr.push(tempArr);
-        else graph[loc].forEach(next => {
-            if (!tempArr.includes(next)) recursive(next, [...tempArr, next]);
-        })
+        if (loc === graph.length - 1) allArr.push([...tempArr, loc]);
+        for (const num of graph[loc]) recursive(num, [...tempArr, loc]);
     }
-    recursive(0, [0]);
+    recursive(0, []);
     return allArr;
 };
