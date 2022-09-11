487c83cb158a0c9bb4656c5490d4dc66b950b34d
Sungjoong Kim <sjjoseph106@gmail.com>
2022-09-11 12:04:43 +0900

    Time: 203 ms (11.60%), Space: 46.3 MB (82.67%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const moveZeroes = function(nums) {
+    let count = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 0) {
+            nums.splice(i, 1);
+            i--;
+            count++;
+        } 
+    }
+    for (let i = 0; i < count; i++) nums.push(0);
+};
