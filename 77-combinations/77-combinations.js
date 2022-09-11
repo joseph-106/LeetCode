db3c3ce1bbd10a6fdfe36be64f4a8f7874f87043
Sungjoong Kim <sjjoseph106@gmail.com>
2022-09-12 01:01:06 +0900

    Time: 186 ms (48.61%), Space: 48.3 MB (64.69%) - LeetHub

--- 77-combinations/77-combinations.js
+++ 77-combinations/77-combinations.js
@@ -6,8 +6,12 @@
 const combine = function(n, k) {
     const resultArr = [];
     const recursive = (loc, tempArr) => {
-        if (tempArr.length === k) resultArr.push(tempArr);
-        else for (let i = loc; i <= n; i++) recursive(i + 1, [...tempArr, i]);
+        if (tempArr.length === k) resultArr.push([...tempArr]);
+        else for (let i = loc; i <= n; i++) {
+            tempArr.push(i);
+            recursive(i + 1, tempArr);
+            tempArr.pop();
+        }
     }
     recursive(1, []);
     return resultArr;
