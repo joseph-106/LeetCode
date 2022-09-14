3a9fdbf839dde59eb7d7d7cf5b786e87e6393760
Sungjoong Kim <sjjoseph106@gmail.com>
2022-09-15 02:25:36 +0900

    Time: 273 ms (15.30%), Space: 58.2 MB (15.21%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.js
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.js
@@ -4,6 +4,6 @@
  */
 const findDuplicates = function(nums) {
     const hashObj = {};
-    for (const num of nums) hashObj[num] = hashObj[num] ? hashObj[num] + 1 : 1;
-    return Object.entries(hashObj).filter(e => e[1] === 2).map(e => e[0]); 
+    for (const num of nums) hashObj[num] ? hashObj[num]++ : hashObj[num] = 1;
+    return Object.entries(hashObj).filter(e => e[1] > 1).map(e => e[0]); 
 };
