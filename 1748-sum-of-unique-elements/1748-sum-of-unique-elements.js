354b5c0d50c69885fb59806f6c878e6b54e3b6a0
Sungjoong Kim <sjjoseph106@gmail.com>
2022-09-20 12:32:13 +0900

    Time: 113 ms (17.17%), Space: 41.9 MB (77.20%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.js
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.js
@@ -5,5 +5,5 @@
 const sumOfUnique = function(nums) {
     const hashObj = {};
     nums.forEach(num => { hashObj[num] ? hashObj[num]++ : hashObj[num] = 1 });
-    return Object.keys(hashObj).filter(key => hashObj[key] === 1).reduce((acc, cur) => acc + +cur, 0);
+    return Object.keys(hashObj).filter(key => hashObj[key] === 1).reduce((acc, cur) => acc + Number(cur), 0);
 };
