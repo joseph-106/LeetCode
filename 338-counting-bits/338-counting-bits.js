0bc93a7b50c771b9c45b254bb9eab04dd17411f1
Sungjoong Kim <sjjoseph106@gmail.com>
2022-09-22 15:19:34 +0900

    Time: 165 ms (34.86%), Space: 53.8 MB (18.99%) - LeetHub

--- 338-counting-bits/338-counting-bits.js
+++ 338-counting-bits/338-counting-bits.js
@@ -4,6 +4,6 @@
  */
 const countBits = function(n) {
     const resultArr = [];
-    for (let i = 0; i <= n; i++) resultArr.push(i.toString(2).replace(/0/g, '').length);
+    for (let i = 0; i <= n; i++) resultArr.push(i.toString(2).replaceAll(0, "").length);
     return resultArr;
 };
