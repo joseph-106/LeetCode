a9229378d42b2a02b89263e1820f33641c47030d
Sungjoong Kim <sjjoseph106@gmail.com>
2022-09-28 12:24:38 +0900

    Time: 121 ms (47.39%), Space: 51.5 MB (44.47%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.js
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.js
@@ -3,9 +3,5 @@
  * @return {number}
  */
 const peakIndexInMountainArray = function(arr) {
-    const len = arr.length;
-    for (let i = 0; i < len; i++) {
-        if (arr[i] > arr[i + 1]) return i;
-        if (arr[len - 1 - i] > arr[len - 2 - i]) return len - 1 - i;
-    } 
+    for (let i = 0; i < arr.length; i++) if (arr[i] > arr[i + 1]) return i;
 };
